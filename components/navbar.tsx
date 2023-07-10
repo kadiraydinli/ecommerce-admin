7712428150cf04223e17c369cb7cab6b83976fe5
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-07-10 20:14:59 +0300

    remove unnecessary from navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,8 +21,6 @@ const Navbar = async () => {
 	return (
 		<div className="border-b">
 			<div className="flex h-16 items-center px-4">
-				<div>This will be a store switcher</div>
-				<div>This will be the routes</div>
 				<StoreSwitcher items={stores} />
 				<MainNav className="mx-6" />
 				<div className="ml-auto flex items-center space-x-4">
